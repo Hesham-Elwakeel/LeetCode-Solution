87b5880ddc9db3eafb288c134292e63b27e52c31
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-06-14 07:58:00 -0700

    Update 7-Reverse-Integer.py

--- Python/7-Reverse-Integer.py
+++ Python/7-Reverse-Integer.py
@@ -18,4 +18,5 @@ Constraints:
 -------------------------------------
 class Solution:
     def reverse(self, x):
-        x = int(str(x)[::-1]) if x >= 0 else int(\-\ + str(x)[::-1][:-1]); return -2 ** 31 <= x <= 2 ** 31 - 1 and x or 0
+        x = int(str(x)[::-1]) if x >= 0 else int(\-\ + str(x)[::-1][:-1]);
+    return -2 ** 31 <= x <= 2 ** 31 - 1 and x or 0
