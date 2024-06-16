cafab0a0288b55b347bd4d75776c4ad0602b4b69
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-06-16 07:34:39 -0700

    [1789] [Time Beats: undefined] - LeetPush

---
+++ SQL/1789-Primary-Department-for-Each-Employee.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+Select employee_id, department_id
+from Employee
+
+where primary_flag = 'Y'
+or
+employee_id in (select employee_id
+    from Employee
+    group by employee_id
+    having count(distinct department_id) = 1)
+
