9a47458e753f6b4f9e3352fb54d3550a4fa72378
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-06-25 07:35:51 -0700

    [34] [Time Beats: 71.75%] [Memory Beats: 30.44%] - LeetPush

---
+++ Python/34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        l, r = bisect.bisect_left(nums, target), bisect.bisect_right(nums, target) - 1
+        return [l, r] if 0 <= l <= r else [-1, -1]
+        
