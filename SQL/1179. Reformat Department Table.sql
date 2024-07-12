ee8984adf0c0e0260ca0edc66cbf2073de98f532
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-07-12 11:09:32 -0700

    Update 1179. Reformat Department Table.sql

--- SQL/1179. Reformat Department Table.sql
+++ SQL/1179. Reformat Department Table.sql
@@ -10,11 +10,8 @@ In SQL,(id, month) is the primary key of this table.
 The table has information about the revenue of each department per month.
 The month has values in ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].
  
-
 Reformat the table such that there is a department id column and a revenue column for each month.
-
 Return the result table in any order.
-
 The result format is in the following example.
 
 Example 1:
