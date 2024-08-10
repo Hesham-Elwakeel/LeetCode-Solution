68d58d984553cd4a3e0bbd0473dc61d95d404449
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-08-10 09:25:23 -0700

    Update 118. Pascal's Triangle.py

--- Python/118. Pascal's Triangle.py
+++ Python/118. Pascal's Triangle.py
@@ -2,7 +2,7 @@ Given an integer numRows, return the first numRows of Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
 
-![image alt](https://github.com/Hesham-Elwakeel/LeetCode-Solution/blob/master/Python/PascalTriangleAnimated2.png)
+![image alt](https://github.com/Hesham-Elwakeel/LeetCode-Solution/blob/master/Python/1-pascals-triangle-science-photo-library.jpg)
 
 Example 1:
 
