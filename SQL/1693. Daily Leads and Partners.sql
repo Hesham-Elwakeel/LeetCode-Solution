c09b1a102e13bfd0f48c4e40f53c1995a7fe6d88
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-08-14 09:28:15 -0700

    Update 1693. Daily Leads and Partners.sql

--- SQL/1693. Daily Leads and Partners.sql
+++ SQL/1693. Daily Leads and Partners.sql
@@ -11,9 +11,7 @@ There is no primary key (column with unique values) for this table. It may conta
 This table contains the date and the name of the product sold and the IDs of the lead and partner it was sold to.
 The name consists of only lowercase English letters.
  
-
 For each date_id and make_name, find the number of distinct lead_id's and distinct partner_id's.
-
 Return the result table in any order.
 
 The result format is in the following example.
