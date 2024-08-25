0278b199601313eb765e7775319a640593a12e6c
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-08-25 13:04:15 -0700

    Update 225. Implement Stack using Queues.py

--- Python/225. Implement Stack using Queues.py
+++ Python/225. Implement Stack using Queues.py
@@ -39,23 +39,32 @@ Follow-up: Can you implement the stack using only one queue?
 
 class MyStack:
     def __init__(self):
+        # This line initializes an empty deque
         self.q1 = deque()
         self.q2 = deque()
-
+    # This method pushes an element x onto the stack.
     def push(self, x: int):
         self.q2.append(x)
-        while self.q1:
+        while self.q1: #that continues as long as there are elements in q1.
+        """
+        Inside the loop, elements are removed from the front of q1 
+        and added to the end of q2. This effectively moves all elements
+        from  q1 to q2, with the newly added element x at the front of q2.
+        """
             self.q2.append(self.q1.popleft())
-        # # Swap the references of the two queues
+        """
+        Swap the references of the two queues
+        his line swaps the references of q1 and q2. Now, q1 contains all the elements, with x at the front, and q2 is empty.
+        """
         self.q1, self.q2 = self.q2, self.q1
 
-    def pop(self):
+    def pop(self): #This method removes and returns the top element of the stack.
         return self.q1.popleft()
 
-    def top(self):
+    def top(self): #This method returns the top element of the stack without removing it.
         return self.q1[0]
 
-    def empty(self):
+    def empty(self): #This method checks whether the stack is empty.
         return len(self.q1) == 0
+# deque, allows you to add or remove values from both ends of a linear queue
 
- # deque, allows you to add or remove values from both ends of a linear queue
