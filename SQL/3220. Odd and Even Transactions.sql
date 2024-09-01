6e2587f4f66bfc5012b575e2ffbdf584d863c80b
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-09-01 08:28:29 -0700

    Update 3220. Odd and Even Transactions.sql

--- SQL/3220. Odd and Even Transactions.sql
+++ SQL/3220. Odd and Even Transactions.sql
@@ -11,7 +11,6 @@ Each row of this table contains the transaction id, amount and transaction date.
 Write a solution to find the sum of amounts for odd and even transactions for each day. If there are no odd or even transactions for a specific date, display as 0.
 
 Return the result table ordered by transaction_date in ascending order.
-
 The result format is in the following example.
  
 Example:
