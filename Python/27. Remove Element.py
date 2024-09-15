9ba1a3571a1fde5fb39e2a237128542e2cd91136
‪Hesham Elwakeel‬‏ <102254636+Hesham-Elwakeel@users.noreply.github.com>
2024-09-15 07:56:48 -0700

    Update 27. Remove Element.py

--- Python/27. Remove Element.py
+++ Python/27. Remove Element.py
@@ -45,3 +45,11 @@ Constraints:
 0 <= nums[i] <= 50
 0 <= val <= 100
 
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        for num in nums:
+            if num != val:
+                nums[i] = num
+                i += 1
+        return i
